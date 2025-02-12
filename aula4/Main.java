26acdfde74ae5d418434843430b1cc921410987a
luisitcho <luishenrisc1@gmail.com>
2025-02-12 09:07:21 -0300

    curso java - aula 4

---
+++ aula4/Main.java
@@ -0,0 +1,20 @@
+// Processamento de dados em Java, Casting
+
+package aula4;
+
+public class Main {
+    public static void main(String[] args) {
+        int x, y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+
+        int a, b;
+        double resultado;
+        a = 5;
+        b = 2;
+        resultado = (double) a / b;
+        System.out.println(resultado);
+    }
+}
