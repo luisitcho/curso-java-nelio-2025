070ded26209080fd2befe5aadb70d8548176ab17
luisitcho <luishenrisc1@gmail.com>
2025-05-05 19:20:54 -0300

    curso java - Estrutura condicional (if-else)

---
+++ aula7/Main.java
@@ -0,0 +1,17 @@
+// Estrutura condicional (if-else)
+
+package aula7;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 12;
+
+        if (x > 0 && x < 4) {
+            System.out.println("Bom dia!");
+        } else if (x > 4 && x < 7) {
+            System.out.println("Boa tarde!");
+        } else {
+            System.out.println("Boa noite!");
+        }
+    }
+}
